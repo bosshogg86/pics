8f2300fe57edb5fdf0a0dd9d64a802875f778672
jeff <jeffhogg86@gmail.com>
2021-01-04 00:34:13 -0800

    pics repo

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+require('dotenv').config();
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${API_KEY}`,
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
